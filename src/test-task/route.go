12cb86e2bf5b6b141a0b711dfd3b66f4b6136773
Aleksey Nikitin <imsouz9@gmail.com>
2017-08-19 16:17:17 +0300

    Extract urls

--- src/test-task/route.go
+++ src/test-task/route.go
@@ -1,9 +1,27 @@
 package main
 
+import "encoding/json"
+import "er"
 import "net/http"
 
 type Route struct {}
 
 func (r *Route) ServeHTTP(res http.ResponseWriter, req *http.Request) {
-	res.WriteHeader(http.StatusNotFound)
+	if req.Method != "POST" {
+		res.WriteHeader(http.StatusNotFound); return }
+
+	_, e := extractUrls(req)
+	if e != nil { res.WriteHeader(http.StatusBadRequest); return }
+}
+
+func extractUrls(req *http.Request) ([]string, error) {
+	if req.Header.Get("content-type") != "application/json" {
+		return nil, er.Er(nil, "Expected Content-Type: application/json") }
+
+	d := json.NewDecoder(req.Body)
+	var urls []string
+	if e := d.Decode(&urls); e != nil {
+		return nil, er.Er(e, "Invalid JSON struct or parsing error") }
+
+	return urls, nil
 }
--- src/test-task/route_test.go
+++ src/test-task/route_test.go
@@ -1,5 +1,7 @@
 package main
 
+import "bytes"
+import "github.com/stretchr/testify/assert"
 import "net/http/httptest"
 import "testing"
 
@@ -7,5 +9,35 @@ func TestDefaultRequest(t *testing.T) {
 	req := httptest.NewRequest("GET", "/", nil)
 	res := httptest.NewRecorder()
 	(&Route{}).ServeHTTP(res, req)
-	if res.Code != 404 { t.Fatal("404 expected") }
+	assert.Equal(t, 404, res.Code)
+}
+
+func TestEmptyContentType(t *testing.T) {
+	req := httptest.NewRequest("POST", "/", nil)
+	res := httptest.NewRecorder()
+	(&Route{}).ServeHTTP(res, req)
+	assert.Equal(t, 400, res.Code)
+}
+
+func TestExtractUrls(t *testing.T) {
+	body := bytes.NewBufferString(`["one", "two", "three"]`)
+	req := httptest.NewRequest("POST", "/", body)
+	req.Header.Add("content-type", "application/json")
+	urls, _ := extractUrls(req)
+	assert.Equal(t, []string{"one", "two", "three"}, urls)
+}
+
+func TestExtractUrlsEmptyBody(t *testing.T) {
+	req := httptest.NewRequest("POST", "/", nil)
+	req.Header.Add("content-type", "application/json")
+	_, e := extractUrls(req)
+	assert.Error(t, e)
+}
+
+func TestExtractUrlsInvalidJSONStruct(t *testing.T) {
+	body := bytes.NewBufferString(`[{"one":1}, {"two":2}]`)
+	req := httptest.NewRequest("POST", "/", body)
+	req.Header.Add("content-type", "application/json")
+	_, e := extractUrls(req)
+	assert.Error(t, e)
 }
